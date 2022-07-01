39e5293c740660a961304278e3fad4cc85066ace
Chris Angelico <rosuav@gmail.com>
2022-07-01 21:07:44 +1000

    Lay out the start and end markers

--- clip.c
+++ clip.c
@@ -15,11 +15,27 @@ int main() {
 		if (strm->codecpar->codec_type == AVMEDIA_TYPE_VIDEO) videoidx = i;
 	}
 	if (videoidx == -1) {printf("No video stream\n"); exit(0);}
-	while (1) {
+	AVStream *strm = s->streams[videoidx];
+	//Our "goal" PTS is defined by a timestamp, here given in seconds, divided by the time_base.
+	int start = 166, length = 57; //2:46
+	int64_t goal = start * strm->time_base.den / strm->time_base.num;
+	int64_t end = goal + length * strm->time_base.den / strm->time_base.num;
+	printf("Goal: %ld End: %ld\n", goal, end);
+	enum {SEEKING, TRANSCODING, COPYING, FINISHING} mode = SEEKING;
+	while (mode != FINISHING) {
 		AVPacket pkt;
 		if (av_read_frame(s, &pkt) < 0) break;
 		if (pkt.stream_index == videoidx) {
-			if (pkt.flags && 1) printf("[%d] Frame: %ld->%ld, flg %x\n", pkt.stream_index, pkt.pts, pkt.duration, pkt.flags);
+			if (mode == SEEKING && pkt.pts >= goal) {
+				mode = TRANSCODING;
+				printf("Start frame: %ld\n", pkt.pts);
+			} else if (mode == TRANSCODING && (pkt.flags & 1)) {
+				printf("Got a key frame: %ld\n", pkt.pts);
+				mode = COPYING;
+			} else if (mode == COPYING && pkt.pts >= end) {
+				printf("Finishing! %ld\n", pkt.pts);
+				mode = FINISHING;
+			}
 		}
 		av_packet_unref(&pkt);
 	}
